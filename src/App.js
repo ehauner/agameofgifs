d75ede5555fd57c491e942c7af45e0f0ee05d80a
Jordan Roth <Jordan-AR@hotmail.com>
2017-03-19 02:17:01 -0700

    formatting error

--- src/App.js
+++ src/App.js
@@ -29,9 +29,9 @@ class App extends Component {
     const currentRound = roundsData[roundsData.length - 1];
     this.setState(
       {
-          gifs: currentRound.gifs;
-          gameMaster: currentRound.gameMaster;
-          winningGif: currentRound.winningGif;
+          gifs: currentRound.gifs,
+          gameMaster: currentRound.gameMaster,
+          winningGif: currentRound.winningGif
       }
     );
   }
